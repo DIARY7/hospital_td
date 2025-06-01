b259b6d1b61bb57d8a8b4e311acb4ccacb39587a
DIARY7 <diaryhakeur@gmail.com>
2025-06-01 21:20:36 +0300

    ataovy amzao alony

--- td1/hospital_patient/__manifest__.py
+++ td1/hospital_patient/__manifest__.py
@@ -13,15 +13,15 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','contacts','hospital_staff'],
+    'depends': ['base','contacts','hospital_staff','hospital_disease'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'views/patient_views.xml',
         'data/security.xml',
         'data/patient_data.xml',
         'views/demand_patient_views.xml',
-        # 'views/views.xml',
         # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
